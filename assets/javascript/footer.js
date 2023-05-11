5176c3b88b7ddd5a2bcf8082b389e143e3884492
Tiffany Reyes <tiffanyreyes.tr@outlook.com>
2023-05-11 11:26:17 -0400

    Update footer.js

--- assets/javascript/footer.js
+++ assets/javascript/footer.js
@@ -3,8 +3,8 @@ const footerHTML = `
 <ul>
     <li><i class="fa-sharp fa-solid fa-school"></i><a href="https://www.bellevue.edu/degrees/bachelor/web-development-bs/">Bellevue University's Web Development Program</a></li>
     <li><i class="fa-solid fa-video"></i><a href="https://www.youtube.com/user/bellevueuniversity">Bellevue University's Youtube</a></li>
-    <li><i class="fa-sharp fa-solid fa-user"></i><a href="https://github.com/buwebdev/web-330">Web 330 Class Github Repository</a></li>
-    <li><i class="fa-sharp fa-solid fa-globe"></i><a href="https://github.com/tiffanyreyes/web-330">My Web 330 Github Repository</a></li>
+    <li><i class="fa-sharp fa-solid fa-globe"></i><a href="https://github.com/buwebdev/web-330">Web 330 Class Github Repository</a></li>
+    <li><i class="fa-sharp fa-solid fa-user"></i><a href="https://github.com/tiffanyreyes/web-330">My Web 330 Github Repository</a></li>
 </ul>
 `;
 
