e06d4f71009723e6b1026c254600e2a9ddb734f8
Tiffany Reyes <tiffanyreyes.tr@outlook.com>
2023-08-15 22:38:38 -0400

    revert js navbar

--- assets/javascript/navbar.js
+++ assets/javascript/navbar.js
@@ -1,5 +1,19 @@
-fetch('../../navbar.html')
-.then(res => res.text())
-.then(text => {
-    document.getElementById("nav-container").innerHTML = text;
-});
+const navbarHTML = `
+<nav class="navbar">
+    <a href="index.html">Home</a>
+    <a href="about.html">About</a>
+    <a href="resume.html">Resume</a>
+    <a href="projects.html">Projects</a>
+    <a href="database-diagrams.html">Database Diagrams</a>
+    <a href="api-unit-tests.html">API Unit Tests</a>
+    <details class="dropdown">
+        <summary class="drop-button">DevOps</summary>
+        <div class="dropdown-menu">
+            <a href="web-430/reyes-tech-value-stream.html" class="presentation-link">Tech Value Stream</a>
+            <a href="web-430/reyes-two-pizza-rule.html" class="presentation-link">Two Pizza Team Rule</a>        
+        </div>
+    </details>
+</nav>
+`;
+
+document.getElementById("nav-container").innerHTML = navbarHTML;
