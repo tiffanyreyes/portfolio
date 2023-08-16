e210d7dd2cfc46012874692a50f3a0da043962b1
Tiffany Reyes <tiffanyreyes.tr@outlook.com>
2023-08-15 22:33:57 -0400

    Add two pizza

--- assets/javascript/navbar.js
+++ assets/javascript/navbar.js
@@ -1,13 +1,5 @@
-const navbarHTML = `
-<div id="navbar">
-    <a href="index.html">Home</a>
-    <a href="about.html">About</a>
-    <a href="resume.html">Resume</a>
-    <a href="projects.html">Projects</a>
-    <a href="database-diagrams.html">Database Diagrams</a>
-    <a href="api-unit-tests.html">API Unit Tests</a>
-    <a href="web-430/reyes-tech-value-stream.html">DevOps</a>
-</div>
-`;
-
-document.getElementById("nav-container").innerHTML = navbarHTML;
+fetch('../../navbar.html')
+.then(res => res.text())
+.then(text => {
+    document.getElementById("nav-container").innerHTML = text;
+});
